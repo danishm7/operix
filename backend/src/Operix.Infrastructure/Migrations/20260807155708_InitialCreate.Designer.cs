bdae077891fb22f6e23784bd431e464629c87f67
Danish M <danish7malak@gmail.com>
2026-08-07 21:36:30 +0530

    Add intital dataabse migration

--- backend/src/Operix.Infrastructure/Migrations/20260807155708_InitialCreate.Designer.cs
+++ backend/src/Operix.Infrastructure/Migrations/20260807155708_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Operix.Infrastructure.Data;
@@ -11,9 +12,11 @@ using Operix.Infrastructure.Data;
 namespace Operix.Infrastructure.Migrations
 {
     [DbContext(typeof(OperixDbContext))]
-    partial class OperixDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260807155708_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
